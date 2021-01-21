f5845d624c3fa5cc3343686f4818ea81506fb881
Jong Min Yu <uok0201@gmail.com>
2021-01-21 14:11:11 +0900

    Initial commit

---
+++ src/main/java/study/querydsl/QuerydslStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslStudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslStudyProjectApplication.class, args);
+	}
+
+}
