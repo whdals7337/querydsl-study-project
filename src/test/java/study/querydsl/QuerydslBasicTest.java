069033f2320ac9b87543272b320c65d2cbb35fbd
wellbell <uok02018496@naver.com>
2021-01-21 17:23:21 +0900

    fetch type, sort 실습

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,15 +1,19 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
@@ -66,5 +70,62 @@ public class QuerydslBasicTest {
 
         // then
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() throws Exception {
+        // given
+
+        // when
+        Member findMember = queryFactory
+                .selectFrom(QMember.member)
+                .where(
+                        QMember.member.username.eq("member1"),
+                        QMember.member.age.between(10, 30)
+                ).fetchOne();
+        // then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void queryKinds() throws Exception {
+        // given
+
+        // when
+        Member fetchOne = queryFactory.selectFrom(member).where(member.username.eq("member1")).fetchOne();
+
+        List<Member> fetchList = queryFactory.selectFrom(member).fetch();
+
+        Member memberFirst = queryFactory.selectFrom(QMember.member).fetchFirst();
+
+        QueryResults<Member> results = queryFactory.selectFrom(member).fetchResults();
+        List<Member> content = results.getResults();
+
+        long l = queryFactory.selectFrom(member).fetchCount();
+        System.out.println(l);
+
+        // then
+
+    }
+
+    @Test
+    public void sort() throws Exception {
+        // given
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        // when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        // then
+        for (Member member1 : result) {
+            System.out.println(member1);
+        }
     }
 }
